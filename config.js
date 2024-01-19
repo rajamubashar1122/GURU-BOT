1318e2017771641b067786b53139eefaff1566e7
rajamubashar1122 <157097112+rajamubashar1122@users.noreply.github.com>
2024-01-19 11:33:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923034683109', 'rajamubashar1122', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
